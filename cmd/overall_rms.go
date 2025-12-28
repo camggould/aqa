ffcd960b6b8047b0a851719d01d9d6f69cd8b6e8
Cameron G. Gould <camggould@protonmail.com>
2025-12-28 15:50:56 -0600

    Cleanup rms command to use new pattern

--- cmd/overall_rms.go
+++ cmd/overall_rms.go
@@ -13,21 +13,19 @@ var rmsCmd = &cobra.Command{
 	Short: "Get the RMS of an mp3 file.",
 	Long: "Get the RMS of a provided mp3 file. Must be a valid MP3.",
 	Run: func(cmd *cobra.Command, args []string) {
-		mp3File, err := cmd.Flags().GetString("mp3")
-
-		if err != nil || mp3File == "" {
-			fmt.Printf("Failed to read mp3 flag: %s\n", err)
-			return
-		}
-		rms, err := GetOverallRMS(mp3File)
-
-		if err != nil {
-			fmt.Printf("RMS could not be calculated due to error: %s\n", err)
-			return
-		}
-
-		fmt.Printf("RMS for file %s: %fdB\n", mp3File, rms)
+		fmt.Printf(HandleMp3Analysis(cmd, args, runRmsCommand))
+		return
 	},
+}
+
+func runRmsCommand(cmd *cobra.Command, args []string, mp3File string) string {
+	rms, err := GetOverallRMS(mp3File)
+
+	if err != nil {
+		return fmt.Sprintf("RMS could not be calculated due to error: %s\n", err)
+	}
+
+	return fmt.Sprintf("RMS for file %s: %fdB\n", mp3File, rms)
 }
 
 func init() {
