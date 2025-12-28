cd7a62451b28068405b4b6a93fed8ef953d4207c
Cameron G. Gould <camggould@protonmail.com>
2025-12-28 15:57:17 -0600

    Update RMS ceiling to use new pattern

--- cmd/rms_ceiling.go
+++ cmd/rms_ceiling.go
@@ -12,17 +12,20 @@ var rmsCeilingCmd = &cobra.Command{
 	Use: "rmsCeiling",
 	Short: "Get the RMS ceiling of an mp3 file.",
 	Long: "Get the RMS ceiling of a provided mp3 file. Must be a valid MP3. Calculated based on the loudest 0.5s segment of audio.",
-	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		rmsFloor, err := GetRmsCeiling(args[0])
-
-		if err != nil {
-			fmt.Printf("RMS ceiling could not be calculated due to error: %s\n", err)
-			return
-		}
-
-		fmt.Printf("RMS ceiling for file %s: %fdB\n", args[0], rmsFloor)
+		fmt.Printf(HandleMp3Analysis(cmd, args, runRmsCeilingCommand))
+		return
 	},
+}
+
+func runRmsCeilingCommand(cmd *cobra.Command, args []string, mp3File string) string {
+	rmsFloor, err := GetRmsCeiling(mp3File)
+
+	if err != nil {
+		return fmt.Sprintf("RMS ceiling could not be calculated due to error: %s\n", err)
+	}
+
+	return fmt.Sprintf("RMS ceiling for file %s: %fdB\n", mp3File, rmsFloor)
 }
 
 func init() {
