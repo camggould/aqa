664fd66f5bcb8092d9f926b5a545db9002af7ba0
Cameron G. Gould <camggould@protonmail.com>
2025-12-29 20:17:59 -0600

    Move initialization to a config.go file

---
+++ cmd/config.go
@@ -0,0 +1,23 @@
+package cmd
+
+func init() {
+	configureFlags()
+	configureCommands()
+}
+
+func configureFlags() {
+	rootCmd.PersistentFlags().String("file", "", "the audio file to analyze.")
+	reportCmd.Flags().String("o", "example.html", "the html file to output a report to. Default is 'report.html'.")
+}
+
+/* Configures enabled commands.
+ * Any time a new command is created, it must be added here to be accessible via CLI.
+*/
+func configureCommands() {
+	rootCmd.AddCommand(channelsCmd)
+	rootCmd.AddCommand(rmsCmd)
+	rootCmd.AddCommand(peakLevelCmd)
+	rootCmd.AddCommand(reportCmd)
+	rootCmd.AddCommand(rmsCeilingCmd)
+	rootCmd.AddCommand(rmsFloorCmd)
+}
